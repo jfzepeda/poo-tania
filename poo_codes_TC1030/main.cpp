bd51258ae333ed6b75e443b62a984b6ae95ea557
FlyingMinion10 <zepedajuanfe9@gmail.com>
2025-05-15 17:31:31 -0600

    Initial Commit

---
+++ poo_codes_TC1030/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  poo_codes_TC1030
+//
+//  Created by Juan Felipe Zepeda on 15/05/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
