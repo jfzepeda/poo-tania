87e8f7df2d5addda37e768c949ea4fb7007a7528
FlyingMinion10 <zepedajuanfe9@gmail.com>
2025-06-12 18:16:18 -0600

    v3.0

--- situacion-problema/main.cpp
+++ situacion-problema/main.cpp
@@ -187,17 +187,118 @@ public:
     }
 };
 
+class Filmoteca {
+private:
+    Pelicula* peliculas[100];
+    Serie* series[100];
+    int numPeliculas;
+    int numSeries;
+public:
+    Filmoteca() : numPeliculas(0), numSeries(0) {}
 
+    void agregarPelicula(Pelicula* pelicula) {
+        if (numPeliculas < 100) {
+            peliculas[numPeliculas++] = pelicula;
+        } else {
+            cout << "No se pueden agregar más películas." << endl;
+        }
+    }
+
+    void agregarSerie(Serie* serie) {
+        if (numSeries < 100) {
+            series[numSeries++] = serie;
+        } else {
+            cout << "No se pueden agregar más series." << endl;
+        }
+    };
+
+    void mostrarContenido() const {
+        cout << "\nPelículas:" << endl;
+        for (int i = 0; i < numPeliculas; i++) {
+            peliculas[i]->mostrar();
+        }
+        cout << "\nSeries:" << endl;
+        for (int i = 0; i < numSeries; i++) {
+            series[i]->mostrar();
+        }
+    };
+
+};
 
 int main() {
-    Pelicula peli1(1, "Película de Prueba", 120, "Acción");
+    // géneros permitidos (drama, acción, misterio).
+    Pelicula peli1(1, "Avengers Endgame", 180, "Accion");
+    Pelicula peli2(2, "The Dark Knight", 152, "Accion");
+    Pelicula peli3(3, "Titanic", 195, "Drama");
+    Pelicula peli4(4, "Inception", 148, "Misterio");
+    Pelicula peli5(5, "The Godfather", 175, "Drama");
+    Pelicula peli6(6, "Interstellar", 169, "Misterio");
+
+    Serie serie1(7, "Serie de Drama", 40, "Drama");
+    serie1.agregarEpisodio("Episodio 1");
+    serie1.agregarEpisodio("Episodio 2");
+    serie1.agregarEpisodio("Episodio 3");
+    serie1.agregarEpisodio("Episodio 4");
+    serie1.agregarEpisodio("Episodio 5");
+    serie1.agregarEpisodio("Episodio 6");
+    serie1.agregarEpisodio("Episodio 7");
+    serie1.agregarEpisodio("Episodio 8");
+    serie1.agregarEpisodio("Episodio 9");
+    serie1.agregarEpisodio("Episodio 10");
+    serie1.agregarEpisodio("Episodio 11");
+    serie1.agregarEpisodio("Episodio 12");
+
+    Serie serie2(8, "Serie de Accion", 50, "Accion");
+    serie2.agregarEpisodio("Episodio 1");
+    serie2.agregarEpisodio("Episodio 2");
+    serie2.agregarEpisodio("Episodio 3");
+    serie2.agregarEpisodio("Episodio 4");
+    serie2.agregarEpisodio("Episodio 5");
+    
+    Serie serie3(9, "Serie de Misterio", 45, "Misterio");
+    serie3.agregarEpisodio("Episodio 1");
+    serie3.agregarEpisodio("Episodio 2");
+    serie3.agregarEpisodio("Episodio 3");
+    
+    Serie serie4(10, "Serie de Accion", 55, "Accion");
+    serie4.agregarEpisodio("Episodio 1");
+    serie4.agregarEpisodio("Episodio 2");
+    serie4.agregarEpisodio("Episodio 3");
+
+    while (true)
+    {
+        cout << "\nSeleccione una opción:" << endl;
+        cout << "1. Mostrar Películas" << endl;
+        cout << "2. Mostrar Series" << endl;
+        cout << "3. Salir" << endl;
+        int opcion;
+        cin >> opcion;
+
+        if (opcion == 1) {
+            peli1.mostrar();
+            peli2.mostrar();
+            peli3.mostrar();
+            peli4.mostrar();
+            peli5.mostrar();
+        } else if (opcion == 2) {
+            serie1.mostrar();
+            serie2.mostrar();
+            serie3.mostrar();
+            serie4.mostrar();
+        } else if (opcion == 3) {
+            break;
+        } else {
+            cout << "Opción no válida, intente de nuevo." << endl;
+        }
+        
+    }    
+
     peli1.mostrarCalificacion();
     float nuevaCalificacion;
     cout << "Ingrese una nueva calificación (0-10): ";
     cin >> nuevaCalificacion;
     peli1.calificar(nuevaCalificacion);
-    
-    Serie serie1(2, "Serie de Prueba", 40, "Drama");
+
     serie1.mostrar();
     float nuevaCalificacion;
     cout << "Ingrese una nueva calificación (0-10): ";
